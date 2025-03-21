d2634713964660748fa7ba4aa3d628ed3a0a4161
AG <ayandeep@valuestocks.in>
2025-03-21 17:14:55 +0530

    21-March@5:14pm

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center w-full h-screen">
-     <p className="text-lg">welcome to react</p>
+     <p className="text-lg text-gray-400">welcome to react</p>
     </div>
   );
 }
