d0f2fc52df8702ad277b473e58ceeec4a280ef81
AyandeepVS <ayandeep@valuestocks.in>
2025-04-05 21:45:21 +0530

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-screen">
-      <p className="text-lg text-gray-400">welcome to react</p>
+      <p className="text-lg text-gray-400">welcome to react......</p>
       <p><a href="https://www.youtube.com/watch?v=S7XpTAnSDL4">watch video</a></p>
     </div>
   );
