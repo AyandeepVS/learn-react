bcb9d2cbfe8320a22c39f3d434feb559d633e582
AG <ayandeep@valuestocks.in>
2025-04-07 10:47:07 +0530

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-screen">
       <p className="text-lg text-gray-400">welcome to react......</p>
-      <p><a href="https://www.youtube.com/watch?v=S7XpTAnSDL4">watch video</a></p>
+      <p><a href="https://www.youtube.com/watch?v=S7XpTAnSDL4">watch this video</a></p>
     </div>
   );
 }
